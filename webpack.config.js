15fcc4d98a19b581d13c8d72db22a67b4badb6ca
davidgape89 <davidgape89@gmail.com>
2019-03-10 16:42:20 +0100

    Added some configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,20 @@ const path = require('path');
 const config = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader']
             }
         ]
     }
