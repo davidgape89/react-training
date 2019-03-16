e88c94ead1533e51539ee12ecc34606fa967d47a
davidgape89 <davidgape89@gmail.com>
2019-03-16 12:43:51 +0100

    finished webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const config = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-              test: /\.css$/,
-              use: ['style-loader', 'css-loader']
-            }
-        ]
+const config = (env, options) => {
+    const isProduction = options.mode === "production";
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        mode: isProduction? 'production': 'development',
+        devtool: isProduction? 'none': 'eval',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                // TODO: Production mode for css
+                {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+                }
+            ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true
+        },
+        watch: isProduction? false: true
     }
 };
 module.exports = config;
