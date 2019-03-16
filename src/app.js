e6e7c9679ede1ea655aa4da3bd881634244d3aca
davidgape89 <davidgape89@gmail.com>
2019-03-16 12:44:21 +0100

    included main file

--- src/app.js
+++ src/app.js
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CreateElementComponent } from "./CreateElementComponent";
-import { ClassComponent } from "./ClassComponent";
-import { PureClassComponent } from "./PureClassComponent";
-import { FunctionalComponent } from "./FunctionalComponent";
 
-import "./styles.css";
+import './styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <CreateElementComponent />
-      <ClassComponent />
-      <PureClassComponent />
-      <FunctionalComponent />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+    <p className="text">
+      This is awesome that everything is
+      working so flawlessly
+    </p>
+  </div>
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
