0ed303add39f23a208401d1ab542c244becdd1d6
davidgape89 <davidgape89@gmail.com>
2019-03-19 22:03:15 +0100

    SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SearchBar.scss';
+
+const SearchBar = (props) => (
+  <input type="text" value={props.query} 
+         onChange={props.onChange}
+         onKeyPress={props.onKeyPress} 
+         placeholder={props.placeholder} />
+)
+
+SearchBar.propTypes = {
+  query: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  onKeyPress: PropTypes.func
+};
+
+export {SearchBar};
