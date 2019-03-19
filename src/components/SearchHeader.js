58c3de1f9102a3ce324c2ba6dd4cfd4f7834d60d
davidgape89 <davidgape89@gmail.com>
2019-03-19 22:40:39 +0100

    SearchHeader component

---
+++ src/components/SearchHeader.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {SearchBar} from './SearchBar';
+import {SearchByToggle} from './SearchByToggle';
+
+import './SearchHeader.scss';
+
+export class SearchHeader extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+      searchBy: 'title'
+    }
+  }
+
+  onQueryChange = (event) => {
+    this.setState({query: event.target.value});
+  }
+
+  onKeyPress = (event) => {
+    if(event.key === "Enter") {
+      this.search();
+    }
+  }
+
+  changeSearchBy = (criteria) => {
+    this.setState({searchBy: criteria});
+  }
+
+  search = () => {
+    console.log('Searching...');
+  }
+
+  render() {
+    return (
+      <div className="search-header">
+        <div className="search-header__content">
+          <div className="search-header__title">
+            netflixroulette
+          </div>
+          <div className="search-header__search-bar">
+            <label>FIND YOUR MOVIE</label>
+            <SearchBar query={this.state.query} 
+                       placeholder={'Some movie title or genre'}
+                       onChange={this.onQueryChange}
+                       onKeyPress={this.onKeyPress} />
+          </div>
+          <div className="search-header__bottom-bar">
+            <SearchByToggle value={this.state.searchBy}
+                            onChange={this.changeSearchBy}/>
+            <button className="button button--red" 
+                    onClick={this.search}>SEARCH</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
