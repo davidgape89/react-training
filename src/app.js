9bac5018606e84a116eae499795ad630f4d97f72
davidgape89 <davidgape89@gmail.com>
2019-03-19 22:48:00 +0100

    Add normalize and search header to main file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {SearchHeader} from './components/SearchHeader';
 
+import 'normalize.css/normalize.css';
 import './styles.scss';
 
 const App = () => (
   <div className="App">
-    <h1>Hello World</h1>
-    <p>
-      This is awesome that everything is
-      working so flawlessly
-    </p>
+    <SearchHeader />
   </div>
 );
 
