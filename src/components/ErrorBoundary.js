9d70409698796b00e9b3b3ac7ba2a865978d3b1c
davidgape89 <davidgape89@gmail.com>
2019-03-22 22:55:01 +0100

    error boundary component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './ErrorBoundary.scss';
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log('something');
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log('Error catched');
+    console.error(error);
+    console.log(info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <div className="error-boundary">Something went wrong!</div>
+        </div>
+      );
+    }
+
+    return this.props.children; 
+  }
+}
