7dacbafa69b0c7ab2a3fd2b24a6c555615ca4f5b
davidgape89 <davidgape89@gmail.com>
2019-03-22 22:58:21 +0100

    Result header component

---
+++ src/components/ResultHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {SortByToggle} from './SortByToggle';
+
+import './ResultHeader.scss';
+
+const ResultHeader = ({resultNumber, sortBy, onChange}) => (
+  <div className="result-header">
+    <div className="result-header__number">
+      {resultNumber} {resultNumber > 1? 'movies': 'movie'} found
+    </div>
+    <SortByToggle sortBy={sortBy}
+                  onChange={onChange}/>
+  </div>
+)
+
+ResultHeader.propTypes = {
+  sortBy: PropTypes.string,
+  resultNumber: PropTypes.number,
+  onChange: PropTypes.func
+};
+
+export {ResultHeader};
