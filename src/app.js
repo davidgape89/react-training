2b9efc31159370c35f4207960eea62b216b5f68f
davidgape89 <davidgape89@gmail.com>
2019-03-23 14:33:38 +0100

    Added mock router functionality

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {SearchHeader} from './components/SearchHeader';
+import {ErrorBoundary} from './components/ErrorBoundary';
+import {SearchPage} from './pages/SearchPage';
+import {DetailsPage} from './pages/DetailsPage';
 
 import 'normalize.css/normalize.css';
 import './styles.scss';
 
-const App = () => (
-  <div className="App">
-    <SearchHeader />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // For shocasing purposes
+    this.state = {
+      page: 'details'
+    };
+  }
+
+  onPageChange = (page) => {
+    this.setState({page});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ErrorBoundary>
+          {this.state.page === 'search'? 
+            <SearchPage onPageChange={this.onPageChange}/>: 
+            <DetailsPage onPageChange={this.onPageChange}/>}
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
