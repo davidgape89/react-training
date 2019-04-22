3c1a1fd28e917b82a8621773e74748b97c9ebc1d
davidgape89 <davidgape89@gmail.com>
2019-04-22 18:20:35 +0200

    Included not found page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {Provider} from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ReactDOM from "react-dom";
 import configureStore from './store/configureStore';
 import {ErrorBoundary} from './components/ErrorBoundary';
 import SearchPage from './pages/SearchPage';
 import DetailsPage from './pages/DetailsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 import 'normalize.css/normalize.css';
 import './styles.scss';
@@ -19,9 +20,12 @@ class App extends React.Component {
         <Router>
           <Provider store = {store}>
             <div className="App">
-              <Route exact={true} path={['/','/search']} component={SearchPage} />
-              <Route path="/search/:query" component={SearchPage} />
-              <Route path="/film/:id" component={DetailsPage} />
+              <Switch>
+                <Route exact={true} path={['/','/search']} component={SearchPage} />
+                <Route path="/search/:query" component={SearchPage} />
+                <Route path="/film/:id" component={DetailsPage} />
+                <Route path="*" component={NotFoundPage} />
+              </Switch>
             </div>
           </Provider>
         </Router>
