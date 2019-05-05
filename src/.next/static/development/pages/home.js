65d2e5f9cb9321a397642d777f014fa3d20dd43b
davidgape89 <davidgape89@gmail.com>
2019-05-05 13:09:59 +0200

    Removed unnecessary files

---
+++ src/.next/static/development/pages/home.js
@@ -0,0 +1,82 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/home.js"],{
+
+/***/ "../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=%2Fhome%2Fdavid%2FProjects%2Freact-training%2Fsrc%2Fpages%2Fhome.js!./":
+/*!*************************************************************************************************************************************************************************************!*\
+  !*** ../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=%2Fhome%2Fdavid%2FProjects%2Freact-training%2Fsrc%2Fpages%2Fhome.js ***!
+  \*************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/home", function() {
+      var page = __webpack_require__(/*! ./pages/home.js */ "./pages/home.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/home.js */ "./pages/home.js", function() {
+          if(!next.router.components["/home"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/home.js */ "./pages/home.js")
+          next.router.update("/home", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "../node_modules/react/index.js":
+/*!********************************************************************************************!*\
+  !*** delegated ../node_modules/react/index.js from dll-reference dll_28982789dc7079c9a54f ***!
+  \********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_28982789dc7079c9a54f */ "dll-reference dll_28982789dc7079c9a54f"))("../node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/home.js":
+/*!***********************!*\
+  !*** ./pages/home.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Example");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ 0:
+/*!****************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fhome&absolutePagePath=%2Fhome%2Fdavid%2FProjects%2Freact-training%2Fsrc%2Fpages%2Fhome.js ***!
+  \****************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fhome&absolutePagePath=%2Fhome%2Fdavid%2FProjects%2Freact-training%2Fsrc%2Fpages%2Fhome.js! */"../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=%2Fhome%2Fdavid%2FProjects%2Freact-training%2Fsrc%2Fpages%2Fhome.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_28982789dc7079c9a54f":
+/*!*******************************************!*\
+  !*** external "dll_28982789dc7079c9a54f" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_28982789dc7079c9a54f;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=home.js.map
--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-import React from "react";
-import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import ReactDOM from "react-dom";
-import configureStore from './store/configureStore';
-import {ErrorBoundary} from './components/ErrorBoundary';
-import SearchPage from './pages/SearchPage';
-import DetailsPage from './pages/DetailsPage';
-import NotFoundPage from './pages/NotFoundPage';
+// import React from "react";
+// import {Provider} from 'react-redux';
+// import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+// import ReactDOM from "react-dom";
+// import configureStore from './store/configureStore';
+// import {ErrorBoundary} from './components/ErrorBoundary';
+// import SearchPage from './pages/SearchPage';
+// import DetailsPage from './pages/DetailsPage';
+// import NotFoundPage from './pages/NotFoundPage';
 
-import 'normalize.css/normalize.css';
-import './styles.scss';
+// import 'normalize.css/normalize.css';
+// import './styles.scss';
 
-const store = configureStore();
+// const store = configureStore();
 
-class App extends React.Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <Router>
-          <Provider store = {store}>
-            <div className="App">
-              <Switch>
-                <Route exact={true} path={['/','/search']} component={SearchPage} />
-                <Route path="/search/:query" component={SearchPage} />
-                <Route path="/film/:id" component={DetailsPage} />
-                <Route path="*" component={NotFoundPage} />
-              </Switch>
-            </div>
-          </Provider>
-        </Router>
-      </ErrorBoundary>
-    );
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <ErrorBoundary>
+//         <Router>
+//           <Provider store = {store}>
+//             <div className="App">
+//               <Switch>
+//                 <Route exact={true} path={['/','/search']} component={SearchPage} />
+//                 <Route path="/search/:query" component={SearchPage} />
+//                 <Route path="/film/:id" component={DetailsPage} />
+//                 <Route path="*" component={NotFoundPage} />
+//               </Switch>
+//             </div>
+//           </Provider>
+//         </Router>
+//       </ErrorBoundary>
+//     );
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
