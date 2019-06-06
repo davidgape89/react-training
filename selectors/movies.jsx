565ef5d2503cfd16161ada6bcff81fd83811de05
davidgape89 <davidgape89@gmail.com>
2019-06-06 22:19:47 +0200

    Used reselect for sorting selector

--- selectors/movies.jsx
+++ selectors/movies.jsx
@@ -1,17 +1,28 @@
-export default (movies, sortBy) => {
-  let resultArray = [...movies];
-  if(sortBy === 'release_date') {
+import { createSelector } from 'reselect';
+
+const getMovies = (state) => state.movies;
+
+const getSortBy = (state) => state.filters.sortBy;
+
+const getSortedMovies = createSelector(
+  [getMovies, getSortBy],
+  (movies, sortBy) => {
+    const resultArray = [...movies];
+  
+    if (sortBy === 'release_date') {
+      return resultArray.sort(
+        (movie1, movie2) => {
+          const timestamp1 = new Date(movie1.release_date);
+          const timestamp2 = new Date(movie2.release_date);
+          return timestamp1 < timestamp2 ? 1 : -1;
+        },
+      );
+    }
+  
     return resultArray.sort(
-      (movie1, movie2) => {
-        const timestamp1 = new Date(movie1.release_date);
-        const timestamp2 = new Date(movie2.release_date);
-        return timestamp1 < timestamp2? 1: -1;
-      });
-  } else {
-    return resultArray.sort(
-      (movie1, movie2) => {
-        return movie1.vote_average < movie2.vote_average? 1: -1;
-      }
+      (movie1, movie2) => (movie1.vote_average < movie2.vote_average ? 1 : -1),
     );
   }
-}
+)
+
+export default getSortedMovies;
